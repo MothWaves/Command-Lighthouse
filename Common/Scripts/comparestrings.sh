ed944fa865628b6c444107bd30113baabe673bee
Moth <sackbot2011@hotmail.com>
2024-01-04 10:40:31 +0000

    Light up the Lighthouse

---
+++ Common/Scripts/comparestrings.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+if [ $# -ne 2 ]; then
+    echo "Please provide 2 strings for comparison"
+    exit -1
+fi
+
+STR1="$1"
+STR2="$2"
+
+if [ "$STR1" = "$STR2" ]; then
+    echo "Strings are equal."
+    exit 0
+else
+    echo "Strings are not equal."
+    exit 1
+fi
