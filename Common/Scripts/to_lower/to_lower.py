a805b980567d195ceb3baf5a06a3670815f746c3
Moth <sackbot2011@hotmail.com>
2024-11-07 18:51:17 +0000

    Adds to_lower to repository

---
+++ Common/Scripts/to_lower/to_lower.py
@@ -0,0 +1,7 @@
+# Gets a string and converts it to lowercase.
+# And also uppercase.
+# Simplest script I've ever made.
+
+string = input()
+print("Lowercase: " + string.lower())
+print("Uppercase: " + string.upper())
